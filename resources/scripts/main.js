41cf9eb7f79ed4392878c8c86378e38a96afdecc
Tyler Replogle <tylerreplogle@gmail.com>
2012-12-17 00:08:09 -0700

    added comments
    made it so that when you place an object from the tool bar, it must be in the map

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -22,28 +22,34 @@
 });
 
 var socket = io.connect(serverIP + ':' + window.location.port);
+
+//onces they connected we give them the objects for the map
 socket.on('connect', function(){
       $("#spinner-text").html("Getting Map Data...");
       socket.emit('getElements');
   });
-  
- socket.on('setElements', function (ElementsIn) {
-      $(".gameView").show();
-      $("#connecting").hide();
-      for (var Element in ElementsIn)
-      {
-        renderMapElement(ElementsIn[Element]);
-      }
-      bindUIEvents();
-  });
 
-  socket.on('setElement', function (ElementIn) {
-     renderMapElement(ElementIn);
-   
-  });
+//renders all the elements and shows the game
+socket.on('setElements', function (ElementsIn) {
+    $(".gameView").show();
+    $("#connecting").hide();
+    for (var Element in ElementsIn)
+    {
+      renderMapElement(ElementsIn[Element]);
+    }
+    bindUIEvents();
+});
+
+//this is so we can see when others update the map
+socket.on('setElement', function (ElementIn) {
+   renderMapElement(ElementIn);
+ 
+});
 
 
 function bindUIEvents(){
+
+  //lets the players move the objects to the map
   $( ".tool-item",".tool-bar" ).draggable({
              stop: function( event, ui ) { 
               $(this).attr("style","position: relative;")
@@ -51,35 +57,56 @@ function bindUIEvents(){
              grid: [ 16, 16 ]
     });
 
-$('.map').draggable({
-                  grid: [ 16, 16 ]
-                  
-                });
+  //lets the user maove the map round.
+  $('.map').draggable({
+                    grid: [ 16, 16 ]
+                    
+                  });
 
+ //makes it so when the drop the items over the map view the show up in the map
  $( ".map-view" ).droppable({
         accept: ".tool-item",
         drop: function( event, ui ) {
-           
+          var size = 64;
           var element = ui.draggable.clone();
-          
           var id = new Date().getTime(); 
-          
+          var map = $('.map')
+
+          //adds an id so we know what to change later
           element.attr('id','ME-'+id)
           element.removeClass("tool-item");
-          element.appendTo( ".map" );
-          element.draggable({
-              grid: [ 16, 16 ], 
-              stop: function( event, ui ) { 
-                sendElement(this);
-               }
-            });
+
+          //changes the left and top to make it be placed in the correct spaot
           adjustToMapSpaces(element, ui.draggable.index());
-          sendElement(element);
-          element.css("position","absolute");
-        
+
+          //makes sure that its in side the size of the map
+          //have to use css becuase offset isnt made yet
+          var top = element.css("top"); 
+          var left = element.css("left");
+          
+          //gets ride of the px and makes an int
+          top = parseInt(top.replace('px'));
+          left = parseInt(left.replace('px'));
+          
+          if (top > ( -1 * size) && top < map.height() && left >  ( -1 * size)  && left < map.width())
+          {
+            element.appendTo( ".map" );
+            element.draggable({
+                grid: [ 16, 16 ], 
+                stop: function( event, ui ) { 
+                  sendElement(this);
+                 }
+              });
+           
+            sendElement(element);
+
+            //by default draggable sets to relitive we want absolute
+            element.css("position","absolute");
+          }
         }
   });
 
+  //this is just right now for move incase there isnt any whtiespace
   $(window).keypress(function(e) {
 
     switch(e.which){
@@ -102,6 +129,8 @@ $('.map').draggable({
   });
 }
 
+
+//looks at the keys and move the map 16px at a time
 function moveMap(direction){
 
   var map = $('.map')
@@ -129,6 +158,7 @@ function moveMap(direction){
 
 }
 
+//takes the obejcets itmes and display an object
 function renderMapElement(ElementIn){
     $("#"+ElementIn.id, ".map").remove();
     var jqeryElement = $('<div id="'+ ElementIn.id +'"" class="'+ElementIn.classes+'"> </div>');
@@ -140,10 +170,10 @@ function renderMapElement(ElementIn){
                    }
                 });
     jqeryElement.attr("style",'top: '+ElementIn.top+'; left: '+ElementIn.left+'; position: absolute;');
-   
-
 }
 
+
+//makes it so when itmes are dragged from the ui they get the correct offset
 function adjustToMapSpaces(element, offset){
 	var mapOffset = $('.map').offset();
   var space = 64;
@@ -157,6 +187,8 @@ function adjustToMapSpaces(element, offset){
   $(element).offset({left: left, top: top});
 }
  
+
+ //makes an object on the fly from the dom element and sends it to the server
  function sendElement(element){
   
  	var object = {
